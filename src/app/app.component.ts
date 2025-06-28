3c0d846806e4182010664beb3420f6b53824eacb
Pratik <pikachubhosale96@gmail.com>
2025-06-28 16:50:43 +0530

    added structure of book flight feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookFlightComponent } from './book-flight/book-flight.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BookFlightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/book-flight/book-flight.component.ts
+++ src/app/book-flight/book-flight.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-book-flight',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule
+  ],
   templateUrl: './book-flight.component.html',
   styleUrl: './book-flight.component.scss'
 })
